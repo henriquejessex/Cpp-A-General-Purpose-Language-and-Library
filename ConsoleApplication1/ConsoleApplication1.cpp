884b42f39703a7faa55331f657f6c473ae6eccf1
Jessé Henrique Pereira <henrique.jessex@gmail.com>
2023-06-11 23:47:22 -0300

    console app

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    int i{ 2 };
+    i = 3;
+    i = 2.9;
+    i = -1;
+
+    unsigned int u{ 2 }; 
+    u = -2;
+
+    double d{ 2.23 };
+    i = d;
+    d = i;
+
+    bool flag{ true };
+    flag = false;
+
+
+}
